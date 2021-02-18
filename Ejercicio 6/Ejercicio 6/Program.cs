9c16d246eb71cdd2e7edd31779d819e3cb997680
Miguel Reyes <79011421+3lMibu@users.noreply.github.com>
2021-02-18 17:46:47 -0600

    Add files via upload

---
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            
+            
+            Console.WriteLine(">> Ingrese el numero A: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(">> Ingrese el numero B: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(">> Ingrese el numero C: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a>c)
+            {
+                Console.WriteLine("El numero mayor es A: {0}", a);
+                
+                if (b>c)
+                {
+                    Console.WriteLine("El numero medio es B: {0}", b);
+                    Console.WriteLine("El numero menor es C: {0}", c);
+                }
+                else
+                {
+                    Console.WriteLine("El numero medio es C: {0}", c);
+                    Console.WriteLine("El numero menor es B: {0}", b);
+                }
+            }
+            else
+            {
+                if (b > a && b > c)
+                {
+                    Console.WriteLine("El numero mayor es B: {0}", b);
+
+                    if(a>c)
+                    {
+                        Console.WriteLine("El numero medio es A: {0}", a);
+                        Console.WriteLine("El numero menor es C: {0}", c);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero medio es C: {0}", c);
+                        Console.WriteLine("El numero menor es A: {0}", a);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("El numero mayor es C: {0}", c);
+                    if (a>b)
+                    {
+                        Console.WriteLine("El numero medio es A: {0}", a);
+                        Console.WriteLine("El numero menor es B: {0}", b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero medio es B: {0}", b);
+                        Console.WriteLine("El numero menor es A: {0}", a);
+                    }
+                }
+            }
+
+        }
+    }
+}
