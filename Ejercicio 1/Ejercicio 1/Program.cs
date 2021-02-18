2db7065d1a6807b709ccdec7af2562b1515b6fe4
3lMibu <79011421+3lMibu@users.noreply.github.com>
2021-02-18 01:26:45 -0600

    Add files via upload

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        decimal cantidad;
+        decimal totalConversion;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de millas marinas a convertir: ");
+            decimal cantidad = decimal.Parse(Console.ReadLine());
+
+            decimal totalConversion = cantidad * 1852;
+            Console.WriteLine("Realizando conversión...");
+            Console.WriteLine("{0} Millas Marinas equivalen a {1:N2} Metros", cantidad, totalConversion);
+
+        }
+    }
+}
